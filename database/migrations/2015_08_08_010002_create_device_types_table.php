ed48941e5ef6400ce1249f171099fa0755e8006e
awkwardusername <awkwardusername@gmail.com>
2015-08-08 16:17:12 +0800

    proper migration tables2

--- database/migrations/2015_08_08_010002_create_device_types_table.php
+++ database/migrations/2015_08_08_010002_create_device_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDevicesTypeTable extends Migration
+class CreateDeviceTypesTable extends Migration
 {
     /**
      * Run the migrations.

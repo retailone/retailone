329a5b2c68a90c45c4326219f3c69140d0d2b549
carl alberto <cralberto11@gmail.com>
2015-08-09 07:40:12 +0800

    added front end animation js dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
@@ -36,10 +37,13 @@
             }
 
             .title {
-                font-size: 96px;
+                font-size: 70px;
                 position: absolute;
-                top: 0px;
+                top: 20%;
+                left: 10%;
                 text-align: center;
+                opacity: .9;
+				box-shadow: 0px 0px 20px 20px #DD572D;
             }
             .carousel-inner > .item > img,
 			.carousel-inner > .item > a > img {
@@ -92,7 +96,11 @@
                 <div class="title"><img src="images/logo2.jpg" alt=""></div>
             </div>
         </div>
+        <script>
+			new WOW().init();
+		</script>
 		<script src="{{ asset('js/jquery.js') }}"></script>
 		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+		<script src="{{ asset('js/wow.js') }}"></script>
     </body>
 </html>

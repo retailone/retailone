e20717e49ab1fa0f633664c767ba9dfc80758591
awkwardusername <awkwardusername@gmail.com>
2015-08-08 12:14:26 +0800

    big bang

---
+++ resources/views/backend/admin/stores/index.blade.php
@@ -0,0 +1,13 @@
+@extends('backend.layout')
+
+@section('css')
+
+@stop
+
+@section('contents')
+
+@stop
+
+@section('js')
+
+@stop
---
+++ resources/views/backend/sidebar.blade.php
@@ -0,0 +1,9 @@
+<li class="active">
+    <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+</li>
+<li>
+    <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+</li>
+<li>
+    <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
+</li>

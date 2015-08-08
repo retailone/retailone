6caaf1e97513b053d2b6f0e953126f87e62ef6f5
carl alberto <cralberto11@gmail.com>
2015-08-09 07:26:35 +0800

    Added logo and slider images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Retail One</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
         <style>
             html, body {
@@ -32,14 +37,62 @@
 
             .title {
                 font-size: 96px;
+                position: absolute;
+                top: 0px;
+                text-align: center;
             }
+            .carousel-inner > .item > img,
+			.carousel-inner > .item > a > img {
+			    width: 100%;
+			    margin: auto;
+			}
         </style>
     </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+    <body>	    
+		<div class="container-fluid">
+			<div class="row">
+				<div class="title">Retail One</div>
+	            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarousel" data-slide-to="1"></li>
+				    <li data-target="#myCarousel" data-slide-to="2"></li>
+				  </ol>
+				
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img src="{{ asset('images/head3.jpg') }}" alt="">
+				    </div>
+				
+				    <div class="item">
+				      <img src="images/head4.jpg" alt="">
+				    </div>
+				
+				    <div class="item">
+				      <img src="images/head6.jpg" alt="">
+				    </div>
+				  </div>
+				
+				  <!-- Left and right controls -->
+				  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+			</div>    
+		</div>        
+	    <div class="container-fluid">
+			<div class="row">           
+                <div class="title"><img src="images/logo2.jpg" alt=""></div>
             </div>
         </div>
+		<script src="{{ asset('js/jquery.js') }}"></script>
+		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
     </body>
 </html>

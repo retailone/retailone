9450d391b44eefa3b83ed2ee858ca380268a4f97
awkwardusername <awkwardusername@gmail.com>
2015-08-09 09:12:15 +0800

    fix layout

--- resources/views/backend/admin/stores/show.blade.php
+++ resources/views/backend/admin/stores/show.blade.php
@@ -36,36 +36,63 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h3>Visits</h3>
-			<div id="visits" style="height: 600px;"></div>
+			<div id="visits" style="height: auto;"></div>
 		</div>
 	</div>
 
 @stop
 
 @section('javascript')
-	<script>
-		$.get('/api/query',{
-			period: $("input:radio[name=period]:checked").val(),
-			device_id: '{{ $device->id }}',
-			from: $("#daterange").val(),
-            to: ''
-		}, function(data) {
-			new Morris.Bar({
-				// ID of the element in which to draw the chart.
-				element: 'visits',
-				// Chart data records -- each entry in this array corresponds to a point on
-				// the chart.
-				data: data,
-				// The name of the data record attribute that contains x-values.
-				xkey: 'name',
-				// A list of names of data record attributes that contain y-values.
-				ykeys: ['count_in', 'count_out'],
-				// Labels for the ykeys -- will be displayed when you hover over the
-				// chart.
-				labels: ['In', 'Out']
-			});
-		});
-	</script>
+    <script>
+        var graph;
+
+        var callApi = function() {
+            return $.get('/api/query',{
+                period: $("input:radio[name=period]:checked").val(),
+                device_id: '{{ $device->id }}',
+                from: $("#daterange").val(),
+                to: ''
+            }, function(data) {
+                graph = Morris.Bar({
+                    // ID of the element in which to draw the chart.
+                    element: 'visits',
+                    // Chart data records -- each entry in this array corresponds to a point on
+                    // the chart.
+                    data: data,
+                    // The name of the data record attribute that contains x-values.
+                    xkey: 'name',
+                    // A list of names of data record attributes that contain y-values.
+                    ykeys: ['count_in', 'count_out'],
+                    // Labels for the ykeys -- will be displayed when you hover over the
+                    // chart.
+                    labels: ['In', 'Out']
+                });
+            });
+        };
+
+        var updateData = function() {
+            return $.get('/api/query',{
+                period: $("input:radio[name=period]:checked").val(),
+                device_id: '{{ $device->id }}',
+                from: $("#daterange").val(),
+                to: ''
+            }, function(data) {
+                graph.setData(data)
+            });
+        };
+
+        callApi();
+    </script>
+    <script>
+        $('input:radio[name=period]').on('change', function () {
+            updateData();
+        });
+
+        $('#daterange').on('change', function () {
+            updateData();
+        });
+    </script>
+
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/js/bootstrap-datepicker.min.js"></script>
     <script type="text/javascript">
         $('#daterange').datepicker({
